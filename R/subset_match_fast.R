eb36779a3dda8e84b60a49323ed204fa03074bbc
Arina ten Cate - Hoek <arinetencate-hoek@Arinas-Air-2.fritz.box>
2024-03-22 21:22:15 +0100

    update

--- R/subset_match_fast.R
+++ R/subset_match_fast.R
@@ -6,25 +6,25 @@
 #' @param column Column to check the exact matches in.
 #' @param ids Identifiers to match (character vector).
 #' @param separator The column delimiter of the file.
+#' @param grep If TRUE, use grep instead of awk (for when awk does not return all matches).
 #' @param first_row If TRUE, returns first row as well.
 #'
 #' @return Returns a subset of the file as a matrix.
 #' @export
 
-subset_match_fast <- function(file, column = 1, ids = NULL, separator = '\t', first_row = TRUE){
+subset_match_fast <- function(file, column = 1, ids = NULL, separator = '\t', grep = FALSE, first_row = TRUE){
   stt <- Sys.time()
-  code <- paste0("ids=\"", paste(ids, collapse=' '),"\"\n",
-                 "for id in $ids; do\n echo \"$id\"\n done > id_list.txt")
-  system(code)
-  code <- paste0("awk 'NR==FNR {ids[$0]; next} {id=$", column,"; sub(/^ *| *$/, \"\", id)} id in ids' id_list.txt ", file)
+  write.table(data.frame(ids), file = 'id_list.txt', row.names = FALSE, col.names = FALSE, quote = FALSE)
+  code <- paste0("awk 'NR==FNR {ids[$1] = 1; next} $", column, " in ids' id_list.txt ", file)
+  if(grep) code <- paste0("grep -f id_list.txt ", file)
   print(code)
   res <- system(code, intern=TRUE)
   system('rm id_list.txt')
   ett <- Sys.time()
   print(difftime(ett, stt, units='secs'))
   res <- do.call('rbind', strsplit(res, separator))
-  if(first_row) rbind(colnames_fast(file, separator), res)
-  else res
+  if(grep)
+    res
 }
 
 
